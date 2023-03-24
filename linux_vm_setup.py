abfbb17c5cc5791681a7199ec062f19044fec4b1
Agent859 <128608530+Agent859@users.noreply.github.com>
2023-03-24 08:40:47 -0600

    Update linux_vm_setup.py

--- linux_vm_setup.py
+++ linux_vm_setup.py
@@ -1,13 +1,21 @@
 import os
 
-print("Signing in as root")
-
 test=os.system("""echo Signing in as root
-
 sudo whoami
-
 echo Root Sign-in Successful
-
 echo Updating packages
+sudo apt update
+echo Installing Docker
+sudo apt install docker.io -y
+sudo systemctl enable docker --now
+sudo usermod -aG docker $USER
+echo Docker Install complete.
+echo installing github repositories
+mkdir gihub
+cd github
+git clone https://github.com/JohnHammond/msdt-follina
+git clone https://github.com/paranoidninja/Brute-Ratel-C4-Community-Kit
+git clone https://github.com/H1R0GH057/Anonymous
 
-sudo apt update""")
+
+""")
